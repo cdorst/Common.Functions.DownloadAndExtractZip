095aed48a96e271e407cf0376914807be1d1089a
Christopher Dorst <chris@dorst.email>
2017-12-30 16:06:47 -0600

    Adding internal helpers

---
+++ Common.Functions.DownloadAndExtractZip/TemporaryArtifactDeleter.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace Common.Functions.DownloadAndExtractZip
+{
+    internal static class TemporaryArtifactDeleter
+    {
+        public static void Delete(string downloadedZipPath, string extractPath)
+        {
+            Directory.Delete(extractPath);
+            File.Delete(downloadedZipPath);
+        }
+    }
+}
---
+++ Common.Functions.DownloadAndExtractZip/ZipDownloader.cs
@@ -0,0 +1,18 @@
+﻿using static Common.Functions.DownloadFile.FileDownloader;
+using static Common.Functions.GetRandomAlphaNumericString.StringGenerator;
+using static System.IO.Path;
+
+namespace Common.Functions.DownloadAndExtractZip
+{
+    internal static class ZipDownloader
+    {
+        private const byte FileNameChars = 7;
+
+        public static string DownloadArchive(string uri, string directory)
+        {
+            var downloadPath = Combine(directory, $"{Random(FileNameChars)}.zip");
+            Download(uri, downloadPath);
+            return downloadPath;
+        }
+    }
+}
