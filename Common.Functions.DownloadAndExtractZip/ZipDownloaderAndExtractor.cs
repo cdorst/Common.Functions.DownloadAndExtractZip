e15ce85782bb3e6d55ed63df051f74293a50ff71
Christopher Dorst <chris@dorst.email>
2017-12-30 16:19:42 -0600

    Add ZipDownloaderAndExtractor

---
+++ Common.Functions.DownloadAndExtractZip/ZipDownloaderAndExtractor.cs
@@ -0,0 +1,18 @@
+﻿using static Common.Functions.CopyAndPasteDirectory.DirectoryCopier;
+using static Common.Functions.DownloadAndExtractZip.TemporaryArtifactDeleter;
+using static Common.Functions.DownloadAndExtractZip.ZipDownloader;
+using static Common.Functions.ExtractZipToNewSubdirectory.ZipExtractor;
+
+namespace Common.Functions.DownloadAndExtractZip
+{
+    public static class ZipDownloaderAndExtractor
+    {
+        public static void DownloadAndExtract(string uri, string destination)
+        {
+            var archive = DownloadArchive(uri, destination);
+            var extract = Extract(archive);
+            Copy(extract, destination);
+            Delete(archive, extract);
+        }
+    }
+}
